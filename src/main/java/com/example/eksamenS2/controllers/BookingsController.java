64c1de4ca989de6f5dd38e7d31350ec6f58d1192
Christian <Haugaard>
2020-05-24 15:42:00 +0200

    Trying to import pictures from folder into a carousel

--- src/main/java/com/example/eksamenS2/controllers/BookingsController.java
+++ src/main/java/com/example/eksamenS2/controllers/BookingsController.java
@@ -29,19 +29,20 @@ public class BookingsController {
 
             result.forEach(System.out::println);
 
-            String rep = "src\\main\\resources";
+            String rep = "src\\main\\resources\\static";
             List<String> result2 = new ArrayList<>();
 
 
             for (int i = 0; i < result.size(); i++) {
                 String s = result.get(i);
                 s = s.replace(rep, "..");
+                s = s.replace("\\", "/");
                 result2.add(s);
 
             }
             result2.forEach(System.out::println);
 
-            model.addAttribute("ModelsImg", result);
+            model.addAttribute("ModelsImg", result2);
         } catch (IOException e) {
             e.printStackTrace();
         }

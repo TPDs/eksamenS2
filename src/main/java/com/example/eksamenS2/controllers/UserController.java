71bfee0f50fe9e975b010d5fe694e04a5efb206b
ironfirst <Daniel_petersen_99@msn.com>
2020-05-25 15:29:46 +0200

    bruger html tabel påbegyndt

---
+++ src/main/java/com/example/eksamenS2/controllers/UserController.java
@@ -0,0 +1,8 @@
+package com.example.eksamenS2.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
--- src/main/java/com/example/eksamenS2/repositories/BookingIDRepositoryImpl.java
+++ src/main/java/com/example/eksamenS2/repositories/BookingIDRepositoryImpl.java
@@ -2,8 +2,5 @@ package com.example.eksamenS2.repositories;
 
 public class BookingIDRepositoryImpl {
 
-// jeg forestiller mig at denne klasse næsten kun skal have referancer og så have start og slut dato
-    // så man bruger referancerne fra andre klasser gennem read og compare metoder til at finde de korrekte primary keys
 
-    // opretter et booking objekt med tilhørende referancer og gemmer den i databasen med foreign key og dato start slut.
 }

a6686853d673ec0c4f20f1f1d76746671f28a02a
ironfirst <Daniel_petersen_99@msn.com>
2020-05-26 15:10:18 +0200

    bruger html tabel påbegyndt

--- src/main/java/com/example/eksamenS2/controllers/MotorHomeController.java
+++ src/main/java/com/example/eksamenS2/controllers/MotorHomeController.java
@@ -1,4 +1,20 @@
 package com.example.eksamenS2.controllers;
 
+import com.example.eksamenS2.models.MotorHome;
+import com.example.eksamenS2.repositories.MotorHomeRepositoryImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MotorHomeController {
+
+    private MotorHomeRepositoryImpl motorhomeRep;
+    public MotorHomeController(){motorhomeRep = new MotorHomeRepositoryImpl();}
+
+    @GetMapping("/motorhomes")
+    public String GetMotorhome(MotorHome motorHome){
+        return "/motorhomes/detailMotorhome";
+    }
+
+
 }
